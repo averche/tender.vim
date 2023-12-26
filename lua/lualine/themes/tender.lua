372ff7849f764f336b50c5cbee2ed42d8a28486d
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-12-26 15:40:44 -0500

    fix lualine colors

--- lua/lualine/themes/tender.lua
+++ lua/lualine/themes/tender.lua
@@ -3,45 +3,45 @@ local tender = {}
 local palette = require("tender.palettes.tender")
 
 tender.normal = {
-	a = { bg = palette.blue4, fg = palette.blue1 },
+	a = { bg = palette.blue1, fg = palette.grey3 },
 	b = { bg = palette.bg, fg = palette.blue2 },
-	c = { bg = palette.blue1, fg = palette.grey3 },
+	c = { bg = palette.blue4, fg = palette.blue1 },
 }
 
 tender.insert = {
-	a = { bg = palette.green4, fg = palette.green1 },
+	a = { bg = palette.green1, fg = palette.green4 },
 	b = { bg = palette.bg, fg = palette.green2 },
-	c = { bg = palette.green1, fg = palette.green4 },
+	c = { bg = palette.green4, fg = palette.green1 },
 }
 
 tender.command = {
-	a = { bg = palette.yellow, fg = palette.bg },
+	a = { bg = palette.float, fg = palette.yellow },
 	b = { bg = palette.bg, fg = palette.yellow },
-	c = { bg = palette.float, fg = palette.yellow },
+	c = { bg = palette.yellow, fg = palette.bg },
 }
 
 tender.visual = {
-	a = { bg = palette.bg, fg = palette.yellow2 },
+	a = { bg = palette.yellow2, fg = palette.grey3 },
 	b = { bg = palette.bg, fg = palette.yellow3 },
-	c = { bg = palette.yellow2, fg = palette.grey3 },
+	c = { bg = palette.bg, fg = palette.yellow2 },
 }
 
 tender.replace = {
-	a = { bg = palette.bg, fg = palette.red1 },
+	a = { bg = palette.red1, fg = palette.grey3 },
 	b = { bg = palette.bg, fg = palette.red2 },
-	c = { bg = palette.red1, fg = palette.grey3 },
+	c = { bg = palette.bg, fg = palette.red1 },
 }
 
 tender.terminal = {
-	a = { bg = palette.yellow, fg = palette.bg },
+	a = { bg = palette.float, fg = palette.yellow },
 	b = { bg = palette.bg, fg = palette.yellow },
-	c = { bg = palette.float, fg = palette.yellow },
+	c = { bg = palette.yellow, fg = palette.bg },
 }
 
 tender.inactive = {
-	a = { bg = palette.gandalf, fg = palette.grey2 },
+	a = { bg = palette.gandalf, fg = palette.grey3 },
 	b = { bg = palette.gandalf, fg = palette.grey3 },
-	c = { bg = palette.gandalf, fg = palette.grey3 },
+	c = { bg = palette.gandalf, fg = palette.grey2 },
 }
 
 return tender
