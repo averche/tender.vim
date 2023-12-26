ae73a684427c532970c972583708de118629fe4e
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-12-26 16:07:39 -0500

    fix lualine colors

--- lua/lualine/themes/tender.lua
+++ lua/lualine/themes/tender.lua
@@ -5,7 +5,7 @@ local palette = require("tender.palettes.tender")
 tender.normal = {
 	a = { bg = palette.blue1, fg = palette.grey3 },
 	b = { bg = palette.bg, fg = palette.blue2 },
-	c = { bg = palette.blue4, fg = palette.blue1 },
+	c = { bg = palette.shadow, fg = palette.blue1 },
 }
 
 tender.insert = {
@@ -15,27 +15,27 @@ tender.insert = {
 }
 
 tender.command = {
-	a = { bg = palette.float, fg = palette.yellow },
-	b = { bg = palette.bg, fg = palette.yellow },
-	c = { bg = palette.yellow, fg = palette.bg },
+	a = { bg = palette.yellow2, fg = palette.grey3 },
+	b = { bg = palette.bg, fg = palette.yellow1 },
+	c = { bg = palette.shadow, fg = palette.yellow2 },
 }
 
 tender.visual = {
 	a = { bg = palette.yellow2, fg = palette.grey3 },
-	b = { bg = palette.bg, fg = palette.yellow3 },
-	c = { bg = palette.bg, fg = palette.yellow2 },
+	b = { bg = palette.bg, fg = palette.yellow1 },
+	c = { bg = palette.shadow, fg = palette.yellow2 },
 }
 
 tender.replace = {
 	a = { bg = palette.red1, fg = palette.grey3 },
 	b = { bg = palette.bg, fg = palette.red2 },
-	c = { bg = palette.bg, fg = palette.red1 },
+	c = { bg = palette.shadow, fg = palette.red1 },
 }
 
 tender.terminal = {
-	a = { bg = palette.float, fg = palette.yellow },
-	b = { bg = palette.bg, fg = palette.yellow },
-	c = { bg = palette.yellow, fg = palette.bg },
+	a = { bg = palette.blue1, fg = palette.grey3 },
+	b = { bg = palette.bg, fg = palette.blue2 },
+	c = { bg = palette.shadow, fg = palette.blue1 },
 }
 
 tender.inactive = {
