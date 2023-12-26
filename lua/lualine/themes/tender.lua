4d7cef2f7ed81d55481690eabe89a7ca7a570def
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2023-12-26 16:27:48 -0500

    fix lualine colors

--- lua/lualine/themes/tender.lua
+++ lua/lualine/themes/tender.lua
@@ -4,37 +4,37 @@ local palette = require("tender.palettes.tender")
 
 tender.normal = {
 	a = { bg = palette.blue1, fg = palette.grey3, gui = "bold" },
-	b = { bg = palette.bg, fg = palette.blue2 },
+	b = { bg = palette.bg, fg = palette.blue2, gui = "bold" },
 	c = { bg = palette.grey3, fg = palette.blue1 },
 }
 
 tender.insert = {
-	a = { bg = palette.green1, fg = palette.green4 },
-	b = { bg = palette.bg, fg = palette.green2 },
+	a = { bg = palette.green1, fg = palette.green4, gui = "bold" },
+	b = { bg = palette.bg, fg = palette.green2, gui = "bold" },
 	c = { bg = palette.grey3, fg = palette.green1 },
 }
 
 tender.command = {
-	a = { bg = palette.yellow2, fg = palette.grey3 },
-	b = { bg = palette.bg, fg = palette.yellow1 },
-	c = { bg = palette.grey3, fg = palette.yellow2 },
+	a = { bg = palette.yellow1, fg = palette.grey3, gui = "bold" },
+	b = { bg = palette.bg, fg = palette.yellow2, gui = "bold" },
+	c = { bg = palette.grey3, fg = palette.yellow1 },
 }
 
 tender.visual = {
-	a = { bg = palette.yellow2, fg = palette.grey3 },
-	b = { bg = palette.bg, fg = palette.yellow1 },
-	c = { bg = palette.grey3, fg = palette.yellow2 },
+	a = { bg = palette.yellow1, fg = palette.grey3, gui = "bold" },
+	b = { bg = palette.bg, fg = palette.yellow2, gui = "bold" },
+	c = { bg = palette.grey3, fg = palette.yellow1 },
 }
 
 tender.replace = {
-	a = { bg = palette.red1, fg = palette.grey3 },
-	b = { bg = palette.bg, fg = palette.red2 },
+	a = { bg = palette.red1, fg = palette.grey3, gui = "bold" },
+	b = { bg = palette.bg, fg = palette.red2, gui = "bold" },
 	c = { bg = palette.grey3, fg = palette.red1 },
 }
 
 tender.terminal = {
-	a = { bg = palette.blue1, fg = palette.grey3 },
-	b = { bg = palette.bg, fg = palette.blue2 },
+	a = { bg = palette.blue1, fg = palette.grey3, gui = "bold" },
+	b = { bg = palette.bg, fg = palette.blue2, gui = "bold" },
 	c = { bg = palette.grey3, fg = palette.blue1 },
 }
 
